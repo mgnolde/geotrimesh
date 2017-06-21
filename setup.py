5170a6cb217a66501e72afacc18d17ff9786bb60
Michael Nolde <mno@flatpolar.org>
2017-06-21 21:57:38 +0200

    edit file structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'geotrimesh',
+  packages = ['geotrimesh'],
+  version = '0.9',
+  description = 'Generator for colored 3D meshes from GIS data',
+  author = 'Michael Nolde',
+  author_email = 'mno@flatpolar.org',
+  url = 'https://github.com/flatpolar/geoTriMesh',
+  download_url = 'https://github.com/flatpolar/geoTriMesh/archive/master.zip',
+  keywords = ['X3D', 'mesh', 'GIS', 'spatial', 'geo'],
+  classifiers = [],
+)
